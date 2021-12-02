57b3db0ab35770672507ffe1ca5a83ab567f3ae2
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2021-12-02 11:57:16 +0100

    Finished product

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.intro', { delay: 1000, distance: '50px', origin: 'top', duration: 2000 });
+ScrollReveal().reveal('.work-1', { delay: 500, distance: '50px', duration: 1000 })
+ScrollReveal().reveal('.work-2', { delay: 800, distance: '50px', duration: 1000 })
+ScrollReveal().reveal('.work-3', { delay: 500, distance: '50px', duration: 1000 })
+ScrollReveal().reveal('.work-4', { delay: 800, distance: '50px', duration: 1000 })
+ScrollReveal().reveal('.work-5', { delay: 500, distance: '50px', duration: 1000 })
+ScrollReveal().reveal('.work-6', { delay: 800, distance: '50px', duration: 1000 })
+
